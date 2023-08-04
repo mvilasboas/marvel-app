ca1f96e130136f246bb3a9cc51ef0fa917a6c819
Marcelo Vilas Boas <ma_net22@hotmail.com>
2023-08-03 22:34:18 -0300

    feat: Criando componente footer, aplicando estilos e ajustando estilos globais

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,18 @@
+import styles from '../../styles/styles.module.css';
+
+export default function Footer() {
+  const COPYRIGHT_YEAR = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerWrapper}>
+        <p>
+          Data provided by Marvel. © {COPYRIGHT_YEAR} MARVEL
+        </p>
+        <p>
+          Desenvolvido por <span>Marcelo Vilas Boas</span>
+        </p>
+      </div>
+    </footer>
+  );
+}
